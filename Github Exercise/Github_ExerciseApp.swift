e0423bda2580a0b1b5982eed9b04910f1f758bf5
rikiitokazu <itokazuriki@gmail.com>
2024-09-01 20:34:47 -0400

    Initial Commit

---
+++ Github Exercise/Github_ExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Github_ExerciseApp.swift
+//  Github Exercise
+//
+//  Created by Riki Itokazu on 9/1/24.
+//
+
+import SwiftUI
+
+@main
+struct Github_ExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
